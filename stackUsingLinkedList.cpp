b2af2460264536270bdc7445ab965f4a819aa163
kalycaokta-ux <kalycaokta@gmail.com>
2026-05-14 11:09:07 +0700

    menagkhiri program utama

--- stackUsingLinkedList.cpp
+++ stackUsingLinkedList.cpp
@@ -103,3 +103,27 @@ int main()
                 else
                 {
                     cout << "Stack is empty. Cannot pop." << endl;
+                }
+                break;
+            case 3:
+                if (!stack.isEmpty())
+                {
+                    stack.peek();
+                }
+                else
+                {
+                    cout << "Stack is empty.No top value." << endl;
+                }
+                break;
+
+            case 4:
+                cout << "Exiting program." << endl;
+                break;
+            default:
+                cout << "invalid choice. Try again." << endl;
+                break;
+        }
+        cout << endl;
+    }
+    return 0;
+};
